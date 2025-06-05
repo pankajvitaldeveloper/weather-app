e88ca19f84a142ea99f96c356900fa47dea28ea8
pankajvitaldeveloper <pankajvitaldeveloper0@example.com>
2025-06-05 16:58:04 +0530

    ui ready

--- src/main.js
+++ src/main.js
@@ -1,1 +1,26 @@
-console.log("hello world");
+const api_key = '2dc189d1166a4e44bc5103522250506';
+const fetchWeatherApi = async ()=> {
+    try{
+        // const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${api_key}&q=London&aqi=no`); 
+        const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=${api_key}&q=Delhi&aqi=no`); 
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        console.log(data);
+        const weather = {
+            location: data.location.name,
+            region: data.location.region,
+            country: data.location.country,
+            temperature: data.current.temp_c,
+            condition: data.current.condition.text,
+            icon: data.current.condition.icon
+        };
+        console.log(weather);
+    }
+    catch(error){
+        console.error("Error fetching weather data:", error);
+    }
+}
+
+fetchWeatherApi();
